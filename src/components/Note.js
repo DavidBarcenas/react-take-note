89191b8ade10243c4695e1b1e58b38a4fa5e429e
David Barcenas <daveepro@outlook.com>
2020-10-19 15:39:49 -0500

    feat: Add CKEditor

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { ArrowBack, Delete, Edit, Label } from '@material-ui/icons';
 import { Button, IconButton, Tooltip } from '@material-ui/core';
 import { useNoteSelector } from '../redux/selectors/notesSelector';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 export const Note = () => {
   const { activeNote } = useNoteSelector();
-
+  const prueba =
+    '<p>Hello from CKEditor 5!dsf</p><ul><li>que hay de new</li><li>por aqui</li></ul>';
   return (
     <div className="note">
       <div className="note__actionbar">
@@ -31,19 +34,27 @@ export const Note = () => {
 
       <div className="note__wrap">
         <h2 className="note__title">Beautiful memories of develop</h2>
-        <p className="note__body">
-          Lorem Ipsum es simplemente el texto de relleno de las imprentas y
-          archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de
-          las industrias desde el año 1500, cuando un impresor (N. del T.
-          persona que se dedica a la imprenta) desconocido usó una galería de
-          textos y los mezcló de tal manera que logró hacer un libro de textos
-          especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como
-          texto de relleno en documentos electrónicos, quedando esencialmente
-          igual al original. Fue popularizado en los 60s con la creación de las
-          hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más
-          recientemente con software de autoedición, como por ejemplo Aldus
-          PageMaker, el cual incluye versiones de Lorem Ipsum.
-        </p>
+
+        {/* <div dangerouslySetInnerHTML={{ __html: prueba }}></div> */}
+
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>Hello from CKEditor 5!</p>"
+          onInit={(editor) => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
       </div>
 
       <div className="note__footer">
