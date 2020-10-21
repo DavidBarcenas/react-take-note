201d406105f07aa80d2b1da5f19b0a19eadfabea
David Barcenas <daveepro@outlook.com>
2020-10-21 10:54:05 -0500

    feat: Add combo to select folder

--- src/components/Note/NoteEdit.js
+++ src/components/Note/NoteEdit.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
-import { Button, TextField } from '@material-ui/core';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from '@material-ui/core';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { useDispatch } from 'react-redux';
 import { saveNewNote } from '../../redux/actions/noteActions';
 import { editorConfig } from '../../util/editorConfig';
+import { AttachFile } from '@material-ui/icons';
 
 export const NoteEdit = () => {
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
   const [value, setValue] = useState({
     title: '',
     body: '',
+    folder: 10,
   });
 
   const handleInputChange = ({ target }) => {
@@ -27,12 +42,27 @@ export const NoteEdit = () => {
     });
   };
 
+  const handleFolderChange = ({ target }) => {
+    if (target.value === 'newFolder') {
+      setOpen(true);
+    } else {
+      setValue({
+        ...value,
+        folder: target.value,
+      });
+    }
+  };
+
   const noteValidation = () => {
     if (value.title.trim() === '' || value.body.trim() === '') {
       return false;
     }
 
     return true;
+  };
+
+  const handleCloseDialog = () => {
+    setOpen(false);
   };
 
   const handleSubmit = () => {
@@ -59,11 +89,67 @@ export const NoteEdit = () => {
         config={editorConfig}
       />
       <div className="note__footer">
-        <Button variant="outlined">Cancelar</Button>
-        <Button className="btn__save" onClick={handleSubmit}>
-          Guardar
-        </Button>
+        <div>
+          <FormControl variant="outlined">
+            <InputLabel id="folder">Carpeta</InputLabel>
+            <Select
+              labelId="folder"
+              value={value.folder}
+              name="folder"
+              onChange={handleFolderChange}
+              label="Carpeta"
+            >
+              <MenuItem value="newFolder">
+                <em>Nueva carpeta</em>
+              </MenuItem>
+              <MenuItem value={10}>General</MenuItem>
+              <MenuItem value={20}>Otro</MenuItem>
+              <MenuItem value={30}>Nanai</MenuItem>
+            </Select>
+          </FormControl>
+          <div>
+            <input accept="image/*" id="icon-button-file" type="file" />
+            <label htmlFor="icon-button-file">
+              <IconButton
+                color="primary"
+                aria-label="upload picture"
+                component="span"
+              >
+                <AttachFile />
+              </IconButton>
+            </label>
+          </div>
+        </div>
+        <div>
+          <Button variant="outlined">Cancelar</Button>
+          <Button
+            variant="contained"
+            className="btn__save"
+            onClick={handleSubmit}
+          >
+            Guardar
+          </Button>
+        </div>
       </div>
+
+      <Dialog aria-labelledby="new-folder" open={open}>
+        <DialogTitle id="new-folder">Crear carpeta</DialogTitle>
+        <DialogContent>
+          <TextField
+            id="outlined-basic"
+            label="Nombre de carpeta"
+            variant="outlined"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleCloseDialog} color="primary">
+            Aceptar
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
