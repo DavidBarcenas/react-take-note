9390a5fd9a7fd3986835babbe10bfb6a41f8a887
David Barcenas <daveepro@outlook.com>
2020-11-09 20:06:03 -0600

    style: Adjust search bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -5,11 +5,12 @@ import { SearchBar } from './SearchBar';
 import Menu from '@material-ui/icons/Menu';
 import SearchOutlined from '@material-ui/icons/SearchOutlined';
 import stickyIcon from '../assets/images/stickyIcon.svg';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logoutApp } from '../redux/actions/authActions';
 
 export const TopBar = () => {
   const dispatch = useDispatch();
+  const { name } = useSelector((state) => state.auth);
   const mq = useMediaQuery('(min-width:767px)');
   const [showSearch, setShowSearch] = useState(false);
 
@@ -17,21 +18,28 @@ export const TopBar = () => {
     <header className="header">
       <div className="topbar">
         <div className="topbar__logo">
-          <IconButton
-            aria-label="delete"
-            size="medium"
-            className="topbar__menu"
-          >
-            <Menu fontSize="inherit" />
-          </IconButton>
+          <div className="logo__wrap">
+            <IconButton
+              aria-label="delete"
+              size="medium"
+              className="topbar__menu"
+            >
+              <Menu fontSize="inherit" />
+            </IconButton>
 
-          <img src={stickyIcon} alt="Take Note" width="30" />
-          <h1>Take Note</h1>
+            <img src={stickyIcon} alt="Take Note" width="30" />
+            <h1>Take Note</h1>
+          </div>
+          {mq && <SearchBar />}
         </div>
 
-        {mq && <SearchBar />}
-
         <div className="topbar__btns">
+          {name && (
+            <div className="user">
+              <span className="square">{name.substr(0, 1)}</span>
+              <span className="user__name">{name}</span>
+            </div>
+          )}
           {!mq && (
             <IconButton
               type="button"
