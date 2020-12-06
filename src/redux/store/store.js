f04f17cb1c03ecbb06dad3ee7d2b76d72950b746
David Barcenas <daveepro@outlook.com>
2020-12-05 20:33:51 -0600

    docs: Update README

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,8 +3,7 @@ import { rootReducer } from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
 
 const composeEnhancers =
-  (typeof window !== 'undefined' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  // (typeof window !== 'undefined' && //   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
 export const store = createStore(
